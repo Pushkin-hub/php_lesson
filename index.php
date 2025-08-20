67e202569aa7ea9c50a51f5fc7538186fd8a331c
Pushkin-hub <pushkinanton2002@gmail.com>
2025-08-20 18:55:44 +0300

    первый коммит

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <p>
+            <?php echo "Hello, world!\n" ?>
+        </p>
+    </div>
+    <div>
+        <li><?php echo "Hello\nWorld!\n" ?></li>
+        <li><?php echo "Hello\n\tworld!" ?></li>
+        <li><?php echo "Hello, world. \"It's my page!\"" ?></li>
+        <p>
+            <?php echo "Hello\nWorld!\n" ?>
+        </p>
+    </div>
+        <div>
+        <p></p>
+        <p></p>
+    </div>
+</body>
+</html>
